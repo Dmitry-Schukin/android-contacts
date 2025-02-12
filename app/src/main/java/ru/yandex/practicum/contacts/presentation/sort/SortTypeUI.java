734dd3ca186cab1a8d3812d98333b44269061937
Dmitry-Schukin <iliakirsanov5321@gmail.com>
2025-02-13 01:10:52 +0300

    Практическая работа №2

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/sort/SortTypeUI.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/sort/SortTypeUI.java
@@ -1,10 +1,10 @@
 package ru.yandex.practicum.contacts.presentation.sort;
 
 import androidx.annotation.NonNull;
-
+import ru.yandex.practicum.contacts.presentation.base.ListDiffCallback;
 import ru.yandex.practicum.contacts.presentation.sort.model.SortType;
 
-public class SortTypeUI {
+public class SortTypeUI implements ListDiffCallback<SortTypeUI> {
 
     private final SortType sortType;
     private final boolean selected;
@@ -20,6 +20,10 @@ public class SortTypeUI {
 
     public boolean isSelected() {
         return selected;
+    }
+    @Override
+    public boolean theSameAs( @NonNull SortTypeUI newItem) {
+        return this.getSortType() == newItem.getSortType();
     }
 
     @Override
@@ -39,4 +43,5 @@ public class SortTypeUI {
         result = 31 * result + (selected ? 1 : 0);
         return result;
     }
+
 }
